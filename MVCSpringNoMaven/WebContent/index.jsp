047a78aa9587802805073ff2ed97e1d3e64b128f
Christian Oscar Tejada Pacheco <zurdosp@gmail.com>
2016-02-23 16:40:48 -0300

    Added files via upload

---
+++ MVCSpringNoMaven/WebContent/index.jsp
@@ -0,0 +1,20 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+ 
+pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Spring 4 MVC - without Maven - ModelAndView Configured</title>
+</head>
+<body>
+ 
+	<center>
+		<h1>Spring 4 MVC - without Maven - ModelAndView Configured</h1>
+		<h2>Hello World :) </h2>
+		<h3>
+			<a href="hello?name=Eric">Click Here</a>
+		</h3>
+	</center>
+</body>
+</html>
---
+++ MVCSpringNoMaven/br.com.testeweb/br/com/testeweb/TesteWeb.java
@@ -0,0 +1,31 @@
+
+package br.com.testeweb;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+ 
+/**
+ * 
+ * @author Christian Oscar Tejada Pacheco
+ *
+ */
+@Controller
+public class TesteWeb {
+
+	String message = "Welcome to Spring MVC!";
+	 
+	@RequestMapping("/hello")
+	public ModelAndView showMessage(
+			@RequestParam(value = "name", required = false, defaultValue = "World") String name) {
+		System.out.println("in controller");
+ 
+		ModelAndView mv = new ModelAndView("helloworld");
+		mv.addObject("message", message);
+		mv.addObject("name", name);
+		return mv;
+	}
+}
+	
+	
